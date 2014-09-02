71ef3990df0ee39b89eb9d434a726108d2a96100
Joshwa Fugett <jfugett27@gmail.com>
2014-09-02 11:49:50 -0700

    chore(work): Committing for work machine

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var paths = {
+  root: __dirname,
+  soften: ['**/*.js', '**/*.json', '!./node_modules/**', '!./docs/**'],
+  src: ['**/*.js', '!./node_modules/**', '!./docs/**'],
+  specs: ['specs/**/*.js'],
+  statements_threshold: 90,
+  functions_threshold: 100,
+  branches_threshold: 100,
+  lines_threshold: 80
+};
+
+require('gulp-module')(gulp, paths);
